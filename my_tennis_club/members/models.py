233c668649d7670886096a26dae51e67e07a6c24
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-20 11:14:46 +0900

    Memberモデルを作成し、初回マイグレーションを生成

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+# データベースの表(テーブル)
 # Create your models here.
 
 class Member(models.Model):
+   # 列名　＝　データ型クラス(VARCHAR(255))
    firstname = models.CharField(max_length=255)
    lastname = models.CharField(max_length=255)
