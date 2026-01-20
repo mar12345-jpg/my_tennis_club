36a38faa76239daf21ee8d2627e65dd7db8bef71
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-20 14:28:46 +0900

    新しいビューを作成

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -16,6 +16,14 @@ def members(request):
    }
    return HttpResponse(template.render(context, request))
 
+def details(request, id):
+   mymember = Member.objects.get(id=id)
+   template = loader.get_template('details.html')
+   context = {
+      'mymember': mymember,
+   }
+   return HttpResponse(template.render(context, request))
+
    # # htmlファイルを探す
    # template = loader.get_template('myfirst.html')
    # # htmlファイルをデータ化して返信する
