82179b2daadf3968781a29afc0a0cf7bd98fa910
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-20 10:21:36 +0900

    renderで今風の書き方に変更

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,13 +1,23 @@
+# 今の書き方　短いよ
+
 from django.shortcuts import render
 
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.あなたのビューをここに書いて
-# パスと連動した関数を書く
-
 def members(request):
-   # htmlファイルを探す
-   template = loader.get_template('myfirst.html')
-   # htmlファイルをデータ化して返信する
-   return HttpResponse(template.render())
+    return render(request, 'myfirst.html')
+
+
+# 下記は昔の書き方
+
+# from django.shortcuts import render
+
+# from django.http import HttpResponse
+# from django.template import loader
+
+# # Create your views here.あなたのビューをここに書いて
+# # パスと連動した関数を書く
+
+# def members(request):
+#    # htmlファイルを探す
+#    template = loader.get_template('myfirst.html')
+#    # htmlファイルをデータ化して返信する
+#    return HttpResponse(template.render())
