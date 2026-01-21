4736e4ba47a628a081e04bb150013b18cae31ce1
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-21 11:46:25 +0900

    文字列表現関数の変更

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -9,6 +9,9 @@ class Member(models.Model):
     phone = models.IntegerField(null=True) # 空データOK
     joined_date = models.DateField(null=True)
 
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
 class Book(models.Model):
     # 列名　＝　データ型クラス(VARCHAR(255))
     title = models.CharField(max_length=255)
