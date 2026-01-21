b45cb77eb7aadb8a019ad8f437ba35e1d79a487a
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-21 13:14:25 +0900

    template.html作成と、ビューで変数を作成

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -30,6 +30,13 @@ def main(request):
    template = loader.get_template('main.html')
    return HttpResponse(template.render())
 
+def testing(request):
+   template = loader.get_template('template.html')
+   context = {
+          'firstname': 'Linus',
+    }
+   return HttpResponse(template.render(context, request))
+
    # # htmlファイルを探す
    # template = loader.get_template('myfirst.html')
    # # htmlファイルをデータ化して返信する
