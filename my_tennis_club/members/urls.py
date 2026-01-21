e88bc7684514154a82eaefd3ac4aeb637215d0d8
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-21 10:42:17 +0900

    メインに戻るリンクを追加

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -4,9 +4,10 @@ from django.urls import path
 from . import views
     
 urlpatterns = [
+    # ドメイン直下名のアドレス
+   path('', views.main, name='main'),
    path('members/', views.members, name='members'),
    path('test/', views.test, name='test'),
    path('hello/', views.hello, name='hello'),
    path('members/details/<int:id>', views.details, name='details'),
-   path('', views.main, name='main'),
 ]
