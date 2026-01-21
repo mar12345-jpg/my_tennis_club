4d718ef278cfaad86c330370a381a7267e2b6348
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-21 13:18:04 +0900

    URL追加

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
    path('test/', views.test, name='test'),
    path('hello/', views.hello, name='hello'),
    path('members/details/<int:id>', views.details, name='details'),
+   path('testing/', views.testing, name='testing'),
 ]
