2825f53574f1ad9566963a767e7fe6c6e13d4dca
mar12345-jpg <o0.marya127.0o@gmail.com>
2026-01-22 11:49:36 +0900

    GET/POSTテスト用ビューとテンプレートを追加

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -5,11 +5,13 @@ from . import views
     
 urlpatterns = [
     # ドメイン直下名のアドレス
-   path('', views.main, name='main'),
-   path('members/', views.members, name='members'),
-   path('test/', views.test, name='test'),
-   path('hello/', views.hello, name='hello'),
-   path('members/details/<int:id>', views.details, name='details'),
-   path('testing/', views.testing, name='testing'),
-   path('members/mypage/', views.mypage, name='mypage'),
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('test/', views.test, name='test'),
+    path('hello/', views.hello, name='hello'),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
+    path('members/mypage/', views.mypage, name='mypage'),
+    # /members/get_post/ にアクセスしたとき get_post 関数を呼び出す
+    path('get_post/', views.get_post, name='get_post'),
 ]
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -3,6 +3,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.shortcuts import render
 
 # Create your views here.あなたのビューをここに書いて
 # パスと連動した関数を書く
@@ -63,6 +64,27 @@ def mypage(request):
         'message': 'ようこそ、マイページへ！',
     }
     return HttpResponse(template.render(context, request))
+
+def get_post(request):
+    # message という変数を初期化（最初は何も入れない）
+    message = None
+
+    # ブラウザから送られてきた HTTP メソッドを判別
+    if request.method == "GET":
+        # GET のときに行う処理
+        # ページを開いたとき（リンクをクリックしたとき）など
+        message = "GET"
+
+    elif request.method == "POST":
+        # POST のときに行う処理
+        # フォームを送信したときなど
+        message = "POST"
+
+    # テンプレートに渡すデータを辞書でまとめる
+    context = {'message': message}
+
+    # get_post.html を表示し、context の内容をテンプレートに渡す
+    return render(request, 'get_post.html', context)
 
 
 # # 今の書き方　テンプレート読み込み → レンダリング → レスポンス をまとめて実行
